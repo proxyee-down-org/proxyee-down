5bcba6fd3c7878afa973081c1453df2da8ff9c4b
nisal hi <nisalhi@nisaldeMacBook-Pro.local>
2018-07-24 22:41:45 +0800

    remove comments

--- front/src/views/Tasks.vue
+++ front/src/views/Tasks.vue
@@ -7,10 +7,6 @@
         <i-button type="dashed" icon="ios-pause" class="tasks-button">{{$t("tasks.pauseDownloads")}}</i-button>
         <i-button type="dashed" icon="ios-trash" class="tasks-button">{{$t("tasks.deleteTask")}}</i-button>
       </div>
-      <!-- <div style="min-height: 200px;">
-        <i-table ref="selection" :columns="taskListsColumns" :data="taskLists" disabled-hover @on-row-click="selectTask"></i-table>
-      </div> -->
-
 
       <tabler 
         :data="data" 
@@ -84,64 +80,6 @@ export default {
           id: 4
         }
       ]
-      // taskListsColumns: [
-      //   {
-      //     type: "selection",
-      //     align: "center",
-      //     width: 60
-      //   },
-      //   {
-      //     title: this.$t("tasks.fileName"),
-      //     key: "fileName",
-      //     width: 200,
-      //     sortable: true
-      //   },
-      //   {
-      //     title: this.$t("tasks.downloadAddress"),
-      //     key: "downloadAddress",
-      //     sortable: true
-      //   },
-      //   {
-      //     title: this.$t("tasks.downloadSpeed"),
-      //     key: "downloadSpeed",
-      //     width: this.$i18n.locale === "zh" ? 110 : 140
-      //   },
-      //   {
-      //     title: this.$t("tasks.expectedCompletionTime"),
-      //     key: "completeTime",
-      //     width: this.$i18n.locale === "zh" ? 150 : 210,
-      //     sortable: true
-      //   },
-      //   {
-      //     title: this.$t("tasks.createTime"),
-      //     key: "createTime",
-      //     width: 150,
-      //     sortable: true
-      //   }
-      // ],
-      // taskLists: [
-      //   {
-      //     fileName: "Windows10Pro.iso",
-      //     downloadAddress: "https://pan.baidu.com/download/win/10/pro.iso",
-      //     downloadSpeed: "20.1M/S",
-      //     completeTime: "2018-7-16 12:20:30",
-      //     createTime: "2018-7-14 12:07:56"
-      //   },
-      //   {
-      //     fileName: "Windows8Pro.iso",
-      //     downloadAddress: "https://pan.baidu.com/10/pro.iso",
-      //     downloadSpeed: "11.7M/S",
-      //     completeTime: "2018-7-16 11:22:31",
-      //     createTime: "2018-7-16 14:07:35"
-      //   },
-      //   {
-      //     fileName: "Pro.iso",
-      //     downloadAddress: "https://pan.baidu.com/win/10/pro.iso",
-      //     downloadSpeed: "21.6M/S",
-      //     completeTime: "2018-7-16 08:21:42",
-      //     createTime: "2018-7-14 08:12:24"
-      //   }
-      // ]
     };
   },
   methods: {
@@ -152,19 +90,6 @@ export default {
     onSelectAll(v) {
       console.log(v);
     }
-
-    // selectTask(currentRow) {
-    // console.log(currentRow);
-    // },
-    // taskProgress() {
-    // let tasksRef = this.$refs.selection;
-    // let tasksTr = tasksRef.$el.querySelectorAll("tr.ivu-table-row");
-
-    // tasksTr.forEach(element => {
-    //   // 给每个tr添加class
-    //   element.className = "taskList";
-    // });
-    // }
   }
 };
 </script>
