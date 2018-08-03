f9b2ee57595bb7cad37b2d5bf5600d9c157656fb
monkeyWie <liwei2633@163.com>
2018-08-03 15:48:29 +0800

    I18N命名规范

---
+++ front/src/i18n/en-US.js
@@ -0,0 +1,32 @@
+export default {
+  nav: {
+    taskManager: 'Task Manager',
+    toolList: 'Tool List',
+    softwareSetting: 'Software Setting',
+    aboutProject: 'About Project',
+    supportUs: 'Support Us'
+  },
+  tasks: {
+    createTasks: 'Create Tasks',
+    continueDownloading: 'Continue Downloading',
+    pauseDownloads: 'Pause Downloads',
+    deleteTask: 'Delete Task',
+    fileName: 'File Name',
+    downloadAddress: 'Download Address',
+    downloadSpeed: 'Download Speed',
+    expectedCompletionTime: 'Expected Completion Time',
+    createTime: 'Create Time',
+    taskProgress: 'Schedule'
+  },
+  alert: {
+    refused: 'Connect Refused',
+    timeout: 'Connect Time Out',
+    error: 'System Error',
+    notFound: 'Task not found',
+    '/tasks': {
+      post: {
+        4007: 'File already exist.'
+      }
+    }
+  }
+}
---
+++ front/src/i18n/zh-CN.js
@@ -0,0 +1,32 @@
+export default {
+  nav: {
+    taskManager: '任务管理',
+    toolList: '工具列表',
+    softwareSetting: '软件设置',
+    aboutProject: '关于项目',
+    supportUs: '支持我们'
+  },
+  tasks: {
+    createTasks: '创建任务',
+    continueDownloading: '继续下载',
+    pauseDownloads: '暂停下载',
+    deleteTask: '删除任务',
+    fileName: '文件名',
+    downloadAddress: '下载地址',
+    downloadSpeed: '下载速度',
+    expectedCompletionTime: '预计完成时间',
+    createTime: '开始时间',
+    taskProgress: '任务进度'
+  },
+  alert: {
+    refused: '拒绝访问',
+    timeout: '连接超时',
+    error: '服务器异常',
+    notFound: '任务不存在',
+    '/tasks': {
+      post: {
+        4007: '文件已存在'
+      }
+    }
+  }
+}
