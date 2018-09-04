6674dce8c31d1645dbb044729d8143fd717f4799
A1anViast <alan9510@163.com>
2018-09-04 18:27:08 +0800

    删除闪一下效果

--- front/src/common/http.js
+++ front/src/common/http.js
@@ -6,18 +6,15 @@ export default {
     const client = axios.create()
     client.interceptors.request.use(
       config => {
-        Vue.prototype.$Spin.show()
         return config
       },
       error => Promise.reject(error)
     )
     client.interceptors.response.use(
       response => {
-        Vue.prototype.$Spin.hide()
         return response
       },
       error => {
-        Vue.prototype.$Spin.hide()
         return Promise.reject(error)
       }
     )
