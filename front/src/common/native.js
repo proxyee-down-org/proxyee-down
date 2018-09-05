5078ac0c3fb40bc8f63291d4e006b2ead323bdee
monkeyWie <liwei2633@163.com>
2018-09-05 11:13:24 +0800

    添加注释

--- front/src/common/native.js
+++ front/src/common/native.js
@@ -40,6 +40,10 @@ const getInitConfig = () => {
   })
 }
 
+/**
+ * 弹出系统资源管理器并选中指定文件
+ * @param {string} path 文件路径
+ */
 const showFile = path => {
   return new Promise((resolve, reject) => {
     client
@@ -49,15 +53,21 @@ const showFile = path => {
   })
 }
 
+/**
+ * 检查证书是否安装
+ */
 const checkCert = () => {
   return new Promise((resolve, reject) => {
-    client
+    clientNoSpin
       .get('/native/checkCert')
       .then(response => resolve(response.data.status))
       .catch(error => reject(error))
   })
 }
 
+/**
+ * 安装证书
+ */
 const installCert = () => {
   return new Promise((resolve, reject) => {
     client
@@ -67,15 +77,22 @@ const installCert = () => {
   })
 }
 
+/**
+ * 取设置的代理模式
+ */
 const getProxyMode = () => {
   return new Promise((resolve, reject) => {
-    client
+    clientNoSpin
       .get('/native/getProxyMode')
       .then(response => resolve(response.data.mode))
       .catch(error => reject(error))
   })
 }
 
+/**
+ * 修改代理模式
+ * @param {number} mode 0.不接管系统代理 1.接管系统代理
+ */
 const changeProxyMode = mode => {
   return new Promise((resolve, reject) => {
     client
@@ -85,15 +102,22 @@ const changeProxyMode = mode => {
   })
 }
 
+/**
+ * 取本地已安装的扩展列表
+ */
 const getExtensions = () => {
   return new Promise((resolve, reject) => {
-    client
+    clientNoSpin
       .get('/native/getExtensions')
       .then(response => resolve(response.data))
       .catch(error => reject(error))
   })
 }
 
+/**
+ * 安装指定扩展
+ * @param {object} data 扩展相关信息
+ */
 const installExtension = data => {
   return new Promise((resolve, reject) => {
     clientNoSpin
@@ -103,6 +127,10 @@ const installExtension = data => {
   })
 }
 
+/**
+ * 更新指定扩展
+ * @param {object} data 扩展相关信息
+ */
 const updateExtension = data => {
   return new Promise((resolve, reject) => {
     clientNoSpin
@@ -112,6 +140,10 @@ const updateExtension = data => {
   })
 }
 
+/**
+ * 启用或禁用扩展
+ * @param {object} data 
+ */
 const toggleExtension = data => {
   return new Promise((resolve, reject) => {
     client
@@ -121,6 +153,10 @@ const toggleExtension = data => {
   })
 }
 
+/**
+ * 打开浏览器并访问指定url
+ * @param {object} data 
+ */
 const openUrl = url => {
   if (window.navigator.userAgent.indexOf('JavaFX') !== -1) {
     clientNoSpin.post('/native/openUrl', { url: encodeURIComponent(url) })
@@ -129,6 +165,10 @@ const openUrl = url => {
   }
 }
 
+/**
+ * 更新软件
+ * @param {string} path 更新包下载地址
+ */
 const doUpdate = path => {
   return new Promise((resolve, reject) => {
     client
@@ -138,10 +178,38 @@ const doUpdate = path => {
   })
 }
 
+/**
+ * 重启软件
+ */
 const doRestart = () => {
   return new Promise((resolve, reject) => {
     client
       .get('/native/doRestart')
+      .then(response => resolve(response.data))
+      .catch(error => reject(error))
+  })
+}
+
+/**
+ * 取软件设置信息
+ */
+const getConfig = () => {
+  return new Promise((resolve, reject) => {
+    clientNoSpin
+      .get('/native/getConfig')
+      .then(response => resolve(response.data))
+      .catch(error => reject(error))
+  })
+}
+
+/**
+ * 保存软件设置
+ * @param {object} config 
+ */
+const setConfig = config => {
+  return new Promise((resolve, reject) => {
+    client
+      .put('/native/setConfig',config)
       .then(response => resolve(response.data))
       .catch(error => reject(error))
   })
@@ -162,3 +230,5 @@ export { getExtensions }
 export { installExtension }
 export { updateExtension }
 export { toggleExtension }
+export { getConfig }
+export { setConfig }
