83edf5233d2a08c60b68f8aaabe2ff9f61b08bf1
monkeyWie <liwei2633@163.com>
2018-09-28 11:08:48 +0800

    移除冗余代码

--- front/src/components/Task/Resolve.vue
+++ front/src/components/Task/Resolve.vue
@@ -21,34 +21,34 @@
         prop="url">
         <Input v-model="form.url" />
       </FormItem>
-      <FormItem :label="$t('tasks.option')">
-        <Checkbox v-model="hasHead">{{ $t('tasks.head') }}</Checkbox>
-        <Checkbox v-model="hasBody">{{ $t('tasks.body') }}</Checkbox>
-      </FormItem>
-      <FormItem v-show="hasHead"
-        :label="$t('tasks.head')"
-        prop="heads">
-        <div v-for="(head, index) in form.heads"
-          :key="index"
-          :class="index === 0 ? null : 'head-margin' ">
-          <Input class="head-input"
+        <FormItem :label="$t('tasks.option')">
+          <Checkbox v-model="hasHead">{{ $t('tasks.head') }}</Checkbox>
+          <Checkbox v-model="hasBody">{{ $t('tasks.body') }}</Checkbox>
+        </FormItem>
+        <FormItem v-show="hasHead"
+          :label="$t('tasks.head')"
+          prop="heads">
+          <div v-for="(head, index) in form.heads"
+            :key="index"
+            :class="index === 0 ? null : 'head-margin' ">
+            <Input class="head-input"
             v-model="head.key"
             placeholder="key" />
-          <Input class="head-input"
+            <Input class="head-input"
             v-model="head.value"
             placeholder="value" />
-          <Icon v-if="index !== 0"
-            type="minus-circled"
-            @click="delHead(index)"></Icon>
-          <Icon v-if="index === form.heads.length - 1"
-            type="plus-circled"
-            @click="addHead"></Icon>
-        </div>
-      </FormItem>
-      <FormItem v-show="hasBody"
-        :label="$t('tasks.body')"
-        prop="body">
-        <Input type="textarea"
+            <Icon v-if="index !== 0"
+              type="minus-circled"
+              @click="delHead(index)"></Icon>
+            <Icon v-if="index === form.heads.length - 1"
+              type="plus-circled"
+              @click="addHead"></Icon>
+          </div>
+        </FormItem>
+        <FormItem v-show="hasBody"
+          :label="$t('tasks.body')"
+          prop="body">
+          <Input type="textarea"
           :autosize="{ minRows: 2, maxRows: 4}"
           v-model="form.body" />
       </FormItem>
@@ -58,9 +58,6 @@
         @click="onSubmit">{{ $t('tip.ok') }}</Button>
       <Button @click="$emit('input', false)">{{ $t('tip.cancel') }}</Button>
     </div>
-    <Spin size="large"
-      fix
-      v-if="load" />
   </Modal>
 </template>
 
@@ -73,7 +70,6 @@ export default {
   },
   data() {
     return {
-      load: false,
       hasHead: false,
       hasBody: false,
       form: {
@@ -124,21 +120,15 @@ export default {
           if (this.hasBody) {
             requestData.body = this.form.body
           }
-          this.load = true
-          this.$http
-            .put('http://127.0.0.1:26339/util/resolve', requestData)
-            .then(result => {
-              this.$emit('input', false)
-              const request = JSON.stringify(result.data.request)
-              const response = JSON.stringify(result.data.response)
-              this.$router.push({
-                path: '/',
-                query: { request: request, response: response }
-              })
+          this.$http.put('http://127.0.0.1:26339/util/resolve', requestData).then(result => {
+            this.$emit('input', false)
+            const request = JSON.stringify(result.data.request)
+            const response = JSON.stringify(result.data.response)
+            this.$router.push({
+              path: '/',
+              query: { request: request, response: response }
             })
-            .finally(() => {
-              this.load = false
-            })
+          })
         }
       })
     },
